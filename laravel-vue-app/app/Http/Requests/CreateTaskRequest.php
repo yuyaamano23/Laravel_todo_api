f56b8fc74276f3c7fda8be99c66a8d92eef9ee90
yuyaamano23 <nba.chako.nba@icloud.com>
2021-01-25 00:47:51 +0900

    make class createtaskrequest

---
+++ laravel-vue-app/app/Http/Requests/CreateTaskRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;  // (1) ここでは認証、認可は不要なので true
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:100', // (2) バリデーションルールを追加
+        ];
+    }
+}
