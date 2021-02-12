053df9c2d15a8aa884ba66798481cc707bdec234
yuyaamano23 <nba.chako.nba@icloud.com>
2021-02-12 15:12:48 +0900

    feat(todoコントローラー): todoCRUDAPIのコントローラー作成
    index,store,show,update,destroyアクション作成

---
+++ laravel-vue-app/app/Http/Controllers/TodoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // todoを作成日時の新しい順で返す
+        return Todo::latest();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // リクエストパラメータから取得した値をもとにTodoを作成する
+        return Todo::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        // オブジェクトを返す
+        return $todo
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        // リクエストパラメータから取得した値をもとにtodoを更新する
+        $todo->update($request->all());
+        return $todo;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        //
+        $todo->delete();
+    }
+}
