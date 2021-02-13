ca68ffe44bfb93e878115af972debc6d49c2d51f
yuyaamano23 <nba.chako.nba@icloud.com>
2021-02-14 02:14:19 +0900

    なんかこれでdeleteのcors直った

--- laravel-vue-app/app/Http/Middleware/Cors.php
+++ laravel-vue-app/app/Http/Middleware/Cors.php
@@ -18,6 +18,6 @@ class Cors
         return $next($request)
         ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type');
+        ->header('Access-Control-Allow-Headers', 'Accept, X-Requested-With, Origin, Content-Type');
     }
 }
