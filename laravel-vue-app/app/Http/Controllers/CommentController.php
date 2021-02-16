06750c781d61be3e05287b46d6f55c05c1781872
yuyaamano23 <nba.chako.nba@icloud.com>
2021-02-16 15:59:20 +0900

    コメントコントローラーの作成

---
+++ laravel-vue-app/app/Http/Controllers/CommentController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // リクエストパラメータから取得した値をもとにコメントを作成する
+        $comment = new Comment;
+        $comment->body = $request->body;
+        $comment->todo_id = $request->todo_id;
+        $comment->save();
+        return $comment;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(comment $comment)
+    {
+        //
+    }
+}
