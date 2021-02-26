742030c72cfd2c8c0b6a0408bf02a031c09a1bd4
yuyaamano23 <nba.chako.nba@icloud.com>
2021-02-27 00:20:19 +0900

    meのルート定義

--- laravel-vue-app/routes/api.php
+++ laravel-vue-app/routes/api.php
@@ -30,6 +30,7 @@ Route::group(['middleware' => ['api', 'cors']], function(){
     Route::post('login', 'Auth\LoginController@login');
     // 認証が必要なapi
     Route::group(["middleware" => "auth:api"], function () {
+    Route::get('me', 'UserController@me');
     // todo関連
     Route::apiResource('todos', 'TodoController');
     // 検索機能API
