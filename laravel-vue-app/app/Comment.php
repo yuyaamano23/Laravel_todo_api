9e8b0df5735bb2225e0fe0958680130325c35dae
yuyaamano23 <nba.chako.nba@icloud.com>
2021-03-04 02:29:01 +0900

    todo,commentモデルのリレーション定義を修正

--- laravel-vue-app/app/Comment.php
+++ laravel-vue-app/app/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    public function comments()
+    public function todo()
     {
         // 多くのCommentsは1つのTodoに関連している
         return $this->belongsTo('App\Todo');
--- laravel-vue-app/app/Todo.php
+++ laravel-vue-app/app/Todo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    public function comments()
+    public function todo()
     {
         // 1つのTodoは多くのcommentを持っている
         return $this->hasMany('App\Comment');
